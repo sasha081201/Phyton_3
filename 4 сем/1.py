784903465f187a0218f7f711833463d8d7919a98
sasha081201 <90838527+sasha081201@users.noreply.github.com>
2022-10-03 18:24:23 +0300

    Add files via upload

---
+++ 4 сем/1.py
@@ -0,0 +1,24 @@
+import sys
+import argparse
+
+def Fibanachi(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return Fibanachi(n - 1) + Fibanachi(n - 2)
+
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', default='10')
+
+    return parser
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        print("kek")
+        sys.exit('Invalid number of arguments')
+    parser = createParser()
+    args = parser.parse_args(sys.argv[1:])
+    print("shit")
+    N = int(sys.argv)
+    print(Fibanachi(N))
