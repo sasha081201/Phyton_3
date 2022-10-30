3609aca5249c6c75350b13d0c9b6d49561d7410f
sasha081201 <90838527+sasha081201@users.noreply.github.com>
2022-10-30 14:20:00 +0300

    Delete 3.py

--- 8 lesson/3.py
+++
@@ -1,8 +0,0 @@
-import itertools
-from timeit import repeat
-from itertools import product
-def get_cartesian_product(a, b):
-    return list(product(a, b))
-    #raise RuntimeError("Not implemented")
-
-print(get_cartesian_product([1, 2], [3, 4])) 
